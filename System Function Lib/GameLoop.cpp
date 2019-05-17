ce1fd31df38eddb960c32e18c64cd3fb1dd60b00
DengYuelin <dengyuelin66dyl@163.com>
2019-05-17 14:14:31 +0800

    update

--- System Function Lib/GameLoop.cpp
+++ System Function Lib/GameLoop.cpp
@@ -1,6 +1,5 @@
 //frame organized and commented by Yuelin Deng
-//This file is used for Auto Control Experiment
-//this should only show on one barch
+//General base file
 #include "DirectX.h"
 #include "Global_variable.h"
 using namespace std;
